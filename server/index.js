e2f970d1455b10df2ce743137f2d75c6e0663b66
Jezrel Dave Sondia <jezreldavesondia@gmail.com>
2024-06-02 04:29:51 +0800

    sales and dashboard functionalities added

--- server/index.js
+++ server/index.js
@@ -26,6 +26,20 @@ db.exec(`
   )
 `);
 
+db.exec(`
+  CREATE TABLE IF NOT EXISTS sales (
+    sale_id INTEGER PRIMARY KEY,
+    product_id INTEGER,
+    name TEXT,
+    quantity INTEGER,
+    cost_price REAL,
+    retail_price REAL,
+    gross_sale REAL,
+    net_sale REAL,
+    sales_date DATE
+  )
+`);
+
 // Define functions for CRUD operations on the database (replace with your schema)
 function createProduct(name, category, stock, costPrice, retailPrice) {
   try {
@@ -45,7 +59,8 @@ function getProduct(productId) {
 
 function updateProduct(productId, name, category, stock, costPrice, retailPrice) {
   try {
-    db.prepare('UPDATE products SET name = ?, category = ?, stock = ?, cost_price = ?, retail_price = ? WHERE product_id = ?').run(name, category, stock, costPrice, retailPrice, productId);
+    db.prepare('UPDATE products SET name = ?, category = ?, stock = ?, cost_price = ?, retail_price = ? WHERE product_id = ?')
+    .run(name, category, stock, costPrice, retailPrice, productId);
   } catch (err) {
     console.error(err);
   }
@@ -53,6 +68,32 @@ function updateProduct(productId, name, category, stock, costPrice, retailPrice)
 
 function deleteProduct(productId) {
   db.prepare('DELETE FROM products WHERE product_id = ?').run(productId);
+}
+
+function createSale(productId, name, quantity, costPrice, retailPrice, grossSale, netSale, salesDate) {
+  try {
+    db.prepare('INSERT INTO sales (product_id, name, quantity, cost_price, retail_price, gross_sale, net_sale, sales_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?)')
+    .run(productId, name, quantity, costPrice, retailPrice, grossSale, netSale, salesDate);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function getSales() {
+  return db.prepare('SELECT * FROM sales').all();
+}
+
+function updateSale(saleId, productId, name, quantity, costPrice, retailPrice, grossSale, netSale, salesDate) {
+  try {
+    db.prepare('UPDATE sales SET product_id = ?, name = ?, quantity = ?, cost_price = ?, retail_price = ?, gross_sale = ?, net_sale = ?, sales_date = ? WHERE sale_id = ?')
+    .run(productId, name, quantity, costPrice, retailPrice, grossSale, netSale, salesDate, saleId);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function deleteSale(saleId) {
+  db.prepare('DELETE FROM sales WHERE sale_id = ?').run(saleId);
 }
 
 // Define API endpoints for CRUD operations (replace with your specific logic)
@@ -83,7 +124,6 @@ app.get('/products/:productId', (req, res) => {
 app.put('/products/:productId', (req, res) => {
   const productId = req.params.productId;
   const { name, category, stock, costPrice, retailPrice } = req.body;
-  console.log({productId, name, category, stock, costPrice, retailPrice})
   updateProduct(productId, name, category, stock, costPrice, retailPrice);
   res.json({ message: 'Product updated successfully', type: 'success' });
 });
@@ -93,7 +133,39 @@ app.post('/deleteProducts', (req, res) => {
   productIdArray.forEach(productId => {
     deleteProduct(productId);
   });
-  res.json({ message: 'Product deleted successfully' });
+  res.json({ message: 'Product deleted successfully', type: 'success' });
+});
+
+//routes for sales
+app.post('/sales', (req, res) => {
+  let { productId, name, quantity, costPrice, retailPrice, grossSale, netSale, salesDate } = req.body;
+  quantity = parseInt(quantity);
+  salesDate = new Date(salesDate);
+  salesDate = salesDate.toISOString().slice(0, 19).replace('T', ' ');
+  createSale(productId, name, quantity, costPrice, retailPrice, grossSale, netSale, salesDate);
+  res.json({ message: 'Sale created successfully', type: 'success' });
+});
+
+app.get('/sales', (req, res) => {
+  const sales = getSales();
+  res.json(sales);
+});
+
+app.put('/sales/:saleId', (req, res) => {
+  const saleId = req.params.saleId;
+  let { productId, name, quantity, costPrice, retailPrice, grossSale, netSale, salesDate } = req.body;
+  salesDate = new Date(salesDate);
+  salesDate = salesDate.toISOString().slice(0, 19).replace('T', ' '); 
+  updateSale(saleId, productId, name, quantity, costPrice, retailPrice, grossSale, netSale, salesDate);
+  res.json({ message: 'Sale updated successfully', type: 'success' });
+});
+
+app.post('/deleteSales', (req, res) => {
+  let saleIdArray = req.body;
+  saleIdArray.forEach(saleId => {
+    deleteSale(saleId);
+  });
+  res.json({ message: 'Sale deleted successfully', type: 'success' });
 });
 
 // Start the server
--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,97 @@
 <template>
   <v-app>
     <v-main>
-      <v-container>
+      <v-container class="position-relative">
         <v-row>
           <h2 class="mt-2">{{ name }} Inventory</h2>
           <v-spacer></v-spacer>
-          <v-alert :text="alertText" :type="alertType" v-if="showAlert"/>
+          <v-alert class="position-fixed" style="z-index:10;top:20px;right:20px" :text="alertText" :type="alertType" v-if="showAlert"/>
         </v-row>
         <v-row>
           <v-col>
             <v-card>
               <v-tabs v-model="tab">
                 <v-tab :value="1">Dashboard</v-tab>
-                <v-tab :value="2">Sales</v-tab>
-                <v-tab :value="3">Stock</v-tab>
+                <v-tab :value="2">Stock</v-tab>
+                <v-tab :value="3">Sales</v-tab>
               </v-tabs>
               <v-tabs-window v-model="tab">
                 <v-tabs-window-item
-                  :value="3"
+                  :value="1"
+                >
+                <v-container>
+                  <!-- <div>
+                    <input type="file" id="input" />
+                  </div> -->
+                  <v-row>
+                    <v-col cols="4">
+                      <v-date-picker
+                        v-model="selectedDate"
+                        color="primary"
+                      ></v-date-picker>
+                    </v-col>
+                    <v-col cols="8">
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12">
+                            <v-container fluid>
+                              <v-row>
+                                <v-col cols="4">
+                                  <v-card color="blue-darken-3" class="px-4 py-4 d-flex flex-column" height="100%">
+                                    <div class="text-h6 font-weight-black mb-5">Monthly Gross Sale</div>
+                                    <div class="text-h4 font-weight-black mb-5 mt-auto">₱{{ summary.monthGross ?  summary.monthGross : 0}}</div>
+                                  </v-card>
+                                </v-col>
+                                <v-col cols="4">
+                                  <v-card color="green-darken-3" class="px-4 py-4 d-flex flex-column" height="100%">
+                                    <div class="text-h6 font-weight-black mb-5">Monthly Net Sale</div>
+                                    <div class="text-h4 font-weight-black mb-5 mt-auto">₱{{ summary.monthNet ?  summary.monthNet : 0}}</div>
+                                  </v-card>
+                                </v-col>
+                                <v-spacer></v-spacer>
+                                <v-col cols="4" align-self="end">
+                                  <v-card color="purple-darken-3" class="px-4 py-4 d-flex flex-column" height="100%">
+                                    <div class="text-h6 font-weight-black mb-5">Total Number of Products</div>
+                                    <div class="text-h4 font-weight-black mb-5 mt-auto">{{ products.length }}</div>
+                                  </v-card>
+                                </v-col>
+                              </v-row>
+                            </v-container>
+                          </v-col>
+                        </v-row>
+                        <v-row>
+                          <v-col cols="12">
+                            <v-container fluid>
+                              <v-row>
+                                <v-col cols="4">
+                                  <v-card color="light-blue-darken-2" class="px-4 py-4 d-flex flex-column" height="100%">
+                                    <div class="text-h6 font-weight-black mb-5">Daily Gross Sale</div>
+                                    <div class="text-h4 font-weight-black mb-5 mt-auto">₱{{ summary.dailyGross ?  summary.dailyGross : 0}}</div>
+                                  </v-card>
+                                </v-col>
+                                <v-col cols="4">
+                                  <v-card color="teal-darken-2" class="px-4 py-4 d-flex flex-column" height="100%">
+                                    <div class="text-h6 font-weight-black mb-5">Daily Net Sale</div>
+                                    <div class="text-h4 font-weight-black mb-5 mt-auto">₱{{ summary.dailyNet ?  summary.dailyNet : 0}}</div>
+                                  </v-card>
+                                </v-col>
+                              </v-row>
+                            </v-container>
+                          </v-col>
+                        </v-row>
+                      </v-container>
+                    </v-col>
+                  </v-row>
+                  
+                  <v-row>
+                    <v-col>
+                      
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-tabs-window-item>
+                <v-tabs-window-item
+                  :value="2"
                 >
                   <v-container fluid>
                     <v-row>
@@ -48,7 +122,7 @@
                         <v-btn v-else block @click="addProduct()" color="primary" size="large">Add Product</v-btn>
                       </v-col>
                       <v-col cols="2" v-if="isEdit">
-                        <v-btn color="warning" @click="isEdit = false" size="large">Cancel Edit</v-btn>
+                        <v-btn color="warning" @click="isEdit = false, newProduct = {}" size="large">Cancel Edit</v-btn>
                       </v-col>
                     </v-row>
                     <v-row>
@@ -58,8 +132,9 @@
                           <v-container>
                             <v-row class="justify-end">
                               <v-col cols="4 d-flex justify-end">
+                                <v-btn color="primary" class="mr-4" @click="openSaleDialog()" :disabled="selectedProducts.length == 1 ? false : true">New Sales</v-btn>
                                 <v-btn color="warning" class="mr-4" @click="isEdit = true, clickEdit()" :disabled="selectedProducts.length == 1 ? false : true">Edit</v-btn>
-                                <v-btn color="error" @click="deleteProducts()">Delete</v-btn>
+                                <v-btn color="error"  @click="deleteProducts()" :disabled="selectedProducts.length < 1 ? true : false">Delete</v-btn>
                               </v-col>
                             </v-row>
                           </v-container>
@@ -97,11 +172,48 @@
                     </v-row>
                   </v-container>
                   </v-tabs-window-item>
+                  <v-tabs-window-item
+                  :value="3"
+                >
+                  <v-container>
+                    <v-row class="justify-end">
+                      <v-col cols="4 d-flex justify-end">
+                        <v-btn color="error" @click="deleteSale()">Delete</v-btn>
+                      </v-col>
+                      </v-row>
+                    <v-data-table 
+                      v-model="selectedSales"
+                      :headers="salesHeaders"
+                      :items="sales"
+                      :search="search"
+                      item-value="name"
+                      return-object
+                      show-select>
+
+                    </v-data-table>
+                  </v-container>
+              </v-tabs-window-item>
               </v-tabs-window>
             </v-card>
           </v-col>
         </v-row>
       </v-container>
+
+      <v-dialog v-model="saleDialog" width="400">
+        <v-card class="px-4 py-4">
+          <div class="text-h5 mb-4">New Sale</div>
+          <div class="text-body-1 mt-3 font-weight-bold mb-2">Product</div>
+          <div class="text-h5 mb-4">{{ newSale.name }}</div>
+          <div class="text-body-1 mt-3 font-weight-bold mb-2">Quantity</div>
+          <div>
+            <v-text-field v-model="newSale.quantity" type="number"/>          
+          </div>
+          <div class="d-flex justify-end">
+            <v-btn color="primary" @click="addSale()">Add Sale</v-btn>
+          </div>
+        </v-card>
+      </v-dialog>
+
     </v-main>
 
     <AppFooter />
@@ -110,23 +222,45 @@
 
 <script>
   import axios from 'axios';
+  import readXlsxFile from 'read-excel-file'
   export default {
     data(){
       return{
         name: 'CDMD Pharmacy',
         apiURL: 'http://localhost:3001',
-        tab: 3,
+        tab: 1,
         selectedProducts: [],
+        selectedSales: [],
         products: [],
+        sales: [],
         search: '',
+        month: '',
+        selectedDate: new Date(),
+        summary: {},
         
         isEdit: false,
+        saleDialog: false,
 
         showAlert: false,
         alertText: '',
         alertType: '',
         
         newProduct: {},
+        newSale: {},
+        months: [
+          'January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December'
+        ],
         categories: [
           "SYRUP",
           "ANTIHYPERTENSIVES",
@@ -138,6 +272,7 @@
           "ANTIHYPERTENSIVE 2",
           "ANTIBIOTICS",
           "CNS/ANTI-VERTIGO/ ANTI-EPILIPTICS",
+          "MEDICAL SUPPLY",
           "MARIDAN PRODUCTS",
           "FOOD SUPP./LINIMENTS/ OTHER OTC",
           "TOPICAL CREAM AND OINTMENTS" ,
@@ -150,25 +285,57 @@
           { title: 'Cost Price', key: 'cost_price' },
           { title: 'Retail Price', key: 'retail_price' },
           { title: 'Stock', key: 'stock' },
-        ]
+        ],
+
+        salesHeaders: [
+          { title: 'Product', key: 'name' },
+          { title: 'Quantity', key: 'quantity' },
+          { title: 'Cost Price', key: 'cost_price' },
+          { title: 'Retail Price', key: 'retail_price' },
+          { title: 'Gross Sale', key: 'gross_sale' },
+          { title: 'Net Sale', key: 'net_sale' },
+          { title: 'Sales Date', key: 'sales_date' },
+        ],
       }
     },
 
     mounted(){
       this.getAllProducts();
+      this.getSales();
+      this.getMonth();
     },
 
     watch: {
-      
+      selectedDate(){
+        this.calculateMonthlyGrossAndNet();
+        this.calculateDailyGrossAndNet();
+      }
     },
 
     methods: {
+
+      /** 
+      populateDatabase(){
+        const input = document.getElementById('input')
+        console.log(input)
+          readXlsxFile(input.files[0]).then((rows) => {
+            rows.forEach((row) => {
+              this.newProduct.name = row[0];
+              this.newProduct.category = row[1];
+              this.newProduct.costPrice = row[2];
+              this.newProduct.retailPrice = row[3];
+              this.newProduct.stock = row[4];
+              this.addProduct();
+            })
+          })
+      },
+      */
+
       getAllProducts(){
         //get all products from the database using axios
         axios.get(this.apiURL + '/products')
         .then(res => {
            this.products = res.data;
-           console.log(this.products);
          });
       },
 
@@ -196,7 +363,6 @@
       editProduct(){
         axios.put(this.apiURL + '/products/' + this.newProduct.product_id, this.newProduct)
         .then(res => {
-          console.log(res)
            this.alertText = res.data.message;
            this.alertType = res.data.type;
            this.showAlert = true;
@@ -210,12 +376,10 @@
       },
 
       deleteProducts(){
-
         let deleteIDArray = [];
         for(let i = 0; i < this.selectedProducts.length; i++){
           deleteIDArray.push(this.selectedProducts[i].product_id);
         }
-        //send selected products to the database using axios
         axios.post(this.apiURL + '/deleteProducts', deleteIDArray).then(res => {
           this.alertText = res.data.message;
           this.alertType = res.data.type;
@@ -226,6 +390,113 @@
           this.selectedProducts = [];
           this.getAllProducts();
         });
+      },
+
+      deleteSale(){
+        let deleteIDArray = [];
+        console.log(this.selectedSales)
+        for(let i = 0; i < this.selectedSales.length; i++){
+          deleteIDArray.push(this.selectedSales[i].sale_id);
+        }
+        axios.post(this.apiURL + '/deleteSales', deleteIDArray)
+        .then(res => {
+           this.alertText = res.data.message;
+           this.alertType = res.data.type;
+           this.showAlert = true;
+           setTimeout(() => {
+             this.showAlert = false;
+           }, 2000);
+           this.selectedSales = [];
+           this.getSales();
+        });
+      },
+
+      openSaleDialog(){
+        this.newSale.name = this.selectedProducts[0].name;
+        this.saleDialog = true;
+      },
+
+
+      getSales(){
+        axios.get(this.apiURL + '/sales')
+        .then(res => {
+           this.sales = res.data;
+           this.calculateMonthlyGrossAndNet();
+           this.calculateDailyGrossAndNet();
+         });
+      },
+
+      addSale(){
+        this.newSale.productId = this.selectedProducts[0].product_id;
+        this.newSale.salesDate = new Date();
+        this.newSale.costPrice = this.selectedProducts[0].cost_price;
+        this.newSale.retailPrice = this.selectedProducts[0].retail_price;
+        this.newSale.grossSale = this.newSale.quantity * this.newSale.retailPrice;
+        this.newSale.netSale =  this.newSale.grossSale - (this.newSale.quantity * this.newSale.costPrice)
+        
+        axios.post(this.apiURL + '/sales', this.newSale)
+        .then(res => {
+           this.alertText = res.data.message;
+           this.alertType = res.data.type;
+           this.showAlert = true;
+           setTimeout(() => {
+             this.showAlert = false;
+           }, 2000);
+           //update the stock of the product
+           let stock = parseInt(this.selectedProducts[0].stock) - parseInt(this.newSale.quantity)
+           let updatedProduct = this.selectedProducts[0]
+           updatedProduct.stock = stock;
+           updatedProduct.costPrice = this.selectedProducts[0].cost_price;
+           updatedProduct.retailPrice = this.selectedProducts[0].retail_price;
+           
+           axios.put(this.apiURL + '/products/' + updatedProduct.product_id, updatedProduct)
+           .then(res => {
+            this.newSale = {};
+            this.getSales();
+            this.saleDialog = false;
+           });
+        });
+      },
+
+      getMonth(){
+        this.month = this.months[new Date().getMonth()];
+      },
+
+      calculateMonthlyGrossAndNet(){
+        let month = this.selectedDate.getMonth();
+        let gross = 0;
+        let net = 0;
+        for(let i = 0; i < this.sales.length; i++){
+           let saleDate = new Date(this.sales[i].sales_date)
+           if(this.months[saleDate.getMonth()] == this.months[month]
+            && saleDate.getFullYear() == this.selectedDate.getFullYear()){
+             gross += this.sales[i].gross_sale;
+             net += this.sales[i].net_sale;
+           }
+
+        }
+        
+        this.summary.monthGross = gross;
+        this.summary.monthNet = net;
+      },
+
+      calculateDailyGrossAndNet(){
+        let day = this.selectedDate.getDate();
+        let gross = 0;
+        let net = 0;
+        for(let i = 0; i < this.sales.length; i++){
+           let saleDate = new Date(this.sales[i].sales_date)
+           if(saleDate.getDate() == day
+            && saleDate.getMonth() == this.selectedDate.getMonth()
+            && saleDate.getFullYear() == this.selectedDate.getFullYear()){
+             gross += this.sales[i].gross_sale;
+             net += this.sales[i].net_sale;
+           }
+        }
+        console.log({net, gross})
+
+        this.summary.dailyGross = gross;
+        this.summary.dailyNet = net;
       }
   }
 }
